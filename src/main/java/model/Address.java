3464875b86397d23878e7dea0ae266a304e24c9b
cph-sl444 <cph-sl444@cphbusiness.dk>
2023-09-04 11:51:08 +0200

    DD
    DDD

--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -1,4 +1,15 @@
 package model;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
 public class Address {
 }
