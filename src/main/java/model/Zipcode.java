a7c4403e196189f0028fa2598742561aa00f0578
veronikaMinddal <cph-vm58@cphbusiness.dk>
2023-09-06 10:48:47 +0200

    ddd

--- src/main/java/model/Zipcode.java
+++ src/main/java/model/Zipcode.java
@@ -32,8 +32,12 @@ public class Zipcode {
     @OneToMany (mappedBy = "zipcode") //cascading? hvad slettes hvis zipcode slettes?
     private Set<Address> addresses = new HashSet<>();
 
-    public Zipcode(int zipcode, String cityName) {
+
+    public Zipcode(int zipcode, String cityName, String regionName, String municipalityName) {
         this.zipcode = zipcode;
         this.cityName = cityName;
+        this.regionName = regionName;
+        this.municipalityName = municipalityName;
     }
+
 }
